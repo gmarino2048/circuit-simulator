273df0d5b88fc3768f74a7a246e0efc29333e944
Guy Marino <gxm262@case.edu>
2021-09-11 21:51:54 -0400

    Add more definitions to transistor

--- inc/circsim/components/Transistor.hpp
+++ inc/circsim/components/Transistor.hpp
@@ -7,15 +7,48 @@
 namespace circsim::components {
 
 class Transistor {
+public:
+
+    typedef Wire::State WireState;
+
 protected:
 
-    size_t _id;
+    ssize_t _id;
     std::string _name;
 
-    Wire *_source;
-    Wire *drain;
-    Wire *_gate;
+    size_t _gate_id;
+    size_t _source_id;
+    size_t _drain_id;
 
+    WireState _gate_state;
+
+    Wire *_source_wire_cached = nullptr;
+    Wire *_drain_wire_cached = nullptr;
+
+public:
+
+    Transistor();
+
+    Transistor(
+        const size_t id,
+        const size_t gate_id,
+        const size_t source_id,
+        const size_t drain_id
+    );
+
+    Transistor(
+        const size_t id,
+        const std::string &name,
+        const size_t gate_id,
+        const size_t source_id,
+        const size_t drain_id
+    );
+
+    WireState gate_state() const;
+    WireState source_state() const;
+    WireState drain_state() const;
+
+    virtual void recalc() = 0;
 
 };
 
