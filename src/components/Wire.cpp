4717d3c215aeaa16556e0c5a7baf882d4962a9ae
Guy Marino <gxm262@case.edu>
2021-10-08 19:56:16 -0400

    Implement wire default constructor

--- src/components/Wire.cpp
+++ src/components/Wire.cpp
@@ -1,2 +1,12 @@
 
 #include <circsim/components/Wire.hpp>
+
+using namespace circsim::components;
+
+Wire::Wire(): _id(-1),
+              _state(UNKNOWN),
+              _externally_driven(false),
+              _driver_function(std::nullopt)
+{
+    // All other fields get default constructor calls
+}
