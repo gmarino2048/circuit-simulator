294883952797e4ce2e33c7c01d4d97d9204f9bc0
Guy Marino <gxm262@case.edu>
2021-10-08 21:08:05 -0400

    Add additional constructor definition

--- src/components/Wire.cpp
+++ src/components/Wire.cpp
@@ -1,8 +1,20 @@
 
+#include <optional>
 #include <circsim/components/Wire.hpp>
 
 using namespace circsim::components;
 
+/// Initialize to -1 until it is set
+ssize_t Wire::_VCC_ID = -1;
+
+/// Initialize to -1 until it is set
+ssize_t Wire::_GND_ID = -1;
+
+
+/**
+ * Default constructor for the wire object. Make sure that the
+ * ID of the wire is -1 so the simulator can quickly detect an error
+ */
 Wire::Wire(): _id(-1),
               _state(UNKNOWN),
               _externally_driven(false),
@@ -10,3 +22,27 @@ Wire::Wire(): _id(-1),
 {
     // All other fields get default constructor calls
 }
+
+
+/**
+ * Construct a special wire object given an ID and a specific type
+ */
+Wire::Wire
+(
+    const size_t id,
+    const SpecialWireType special_type,
+    const std::vector<size_t> &control_transistors,
+    const std::vector<size_t> &gate_transistors
+):  _id(id),
+    _state(UNKNOWN),
+    _externally_driven(true),
+    _trans_ctl_ids(control_transistors),
+    _trans_gate_ids(gate_transistors)
+{
+    _primary_name = special_wire_name(special_type);
+    _driver_function = special_wire_func(special_type);
+    
+    set_special_wire_id(special_type);
+}
+
+   
