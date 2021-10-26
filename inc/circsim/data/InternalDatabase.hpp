d5076352ab26467d3d49f93c938941281c1c8d7a
Guy Marino <gxm262@case.edu>
2021-10-25 20:28:35 -0400

    Create initial internal database file

---
+++ inc/circsim/data/InternalDatabase.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file InternalDatabase.hpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief This file contains the template declaration for
+ *        an internal database of components. Within the
+ *        circuit simulation, there will be two separate
+ *        databases for the transistor and wire elements.
+ * @version 0.1
+ * @date 2021-10-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __CIRCSIM_DATA_INTERNALDATABASE_HPP
+#define __CIRCSIM_DATA_INTERNALDATABASE_HPP
+
+namespace circsim::data
+{
+
+template <class T>
+class InternalDatabase final
+{
+
+    
+
+};
+
+}
+
+#endif //__CIRCSIM_DATA_INTERNALDATABASE_HPP
