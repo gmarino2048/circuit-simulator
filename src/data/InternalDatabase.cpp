dd8389ff3ed5f9a65354287c0095f273194309f0
Guy Marino <gxm262@case.edu>
2021-11-03 20:51:17 -0400

    Add ICD implementation file

---
+++ src/data/InternalDatabase.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file InternalDatabase.cpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief This contains the definition for the internal database
+ *        instance which is responsible for organizing and indexing
+ *        all the components for the simulator
+ * @version 0.1
+ * @date 2021-11-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <circsim/data/InternalDatabase.hpp>
+
