870d584b13f3a21fafdd15694e1919e0669f13ef
Guy Marino <gxm262@case.edu>
2021-11-08 18:22:36 -0500

    Add definition for element indexing

--- src/data/InternalDatabase.cpp
+++ src/data/InternalDatabase.cpp
@@ -11,5 +11,29 @@
  * 
  */
 
+#include <string>
+
 #include <circsim/data/InternalDatabase.hpp>
 
+using Wire = circsim::components::Wire;
+using Transistor = circsim::components::Transistor;
+using InternalDatabase = circsim::data::InternalDatabase;
+
+
+void InternalDatabase::_index_element(const Wire &wire)
+{
+    if( wire.id() < 0 )
+    {
+        throw std::runtime_error
+        (
+            "Expected nonnegative wire ID for object:\n" +
+            static_cast<std::string>(wire)
+        );
+    }
+
+    size_t id = static_cast<size_t>(wire.id());
+    Wire *object_ptr = const_cast<Wire*>(&wire);
+
+    // No need to do boundary checking since this is addition
+    _wire_index[id] = object_ptr;
+}
