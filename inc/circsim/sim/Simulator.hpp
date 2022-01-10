031856097779f84f9f7f3298f53e667e32492c54
Guy Marino <gmarino2048@gmail.com>
2022-01-09 19:57:06 -0500

    Add more documentation

--- inc/circsim/sim/Simulator.hpp
+++ inc/circsim/sim/Simulator.hpp
@@ -56,28 +56,86 @@ private:
     /// The internal component database for the simulator
     data::InternalDatabase _internal_database;
 
+
+    /**
+     * @brief Create a wire group from a given wire ID. The Wire group is
+     *        then filled into the reference provided. This allows a single
+     *        wire group to be reused rather than reallocating memory every
+     *        iteration.
+     * 
+     * @param wire_id The ID of the initial wire used to create the group.
+     * @param group The receiving reference for the wire group.
+     */
     void _create_wire_group(const size_t wire_id, WireGroup &group);
+
+
+    /**
+     * @brief Update all the gate-side transistors from the wire group. If
+     *        the transistor changes state then add the proper wires to the
+     *        update list.
+     * 
+     * @param group The WireGroup used to update the transistors
+     */
     void _update_transistors(const WireGroup &group);
 
 public:
 
+    /**
+     * @brief Default constructor for the Simulator
+     * 
+     * @param iteration_limit The iteration limit for the simulator (default 2,500)
+     */
     Simulator(const size_t iteration_limit = 2500);
 
+    /**
+     * @brief Construct a new Simulator object with an existing component database
+     * 
+     * @param database The component database to use for the simulator
+     * @param iteration_limit The limit of iterations to resolve changes in the simulator (default 2500)
+     */
     Simulator
     (
         const data::InternalDatabase &database,
         const size_t iteration_limit = 2500
     );
 
-    inline data::InternalDatabase& database() { return _internal_database; }
-    inline void database(const data::InternalDatabase &database) { _internal_database = database; }
 
+    /**
+     * @brief Get the internal database of the simulator
+     * 
+     * @return data::InternalDatabase& The internal component database
+     */
+    inline data::InternalDatabase& database() { return _internal_database; }
+
+    /**
+     * @brief Update the internal component database with a newer version.
+     * 
+     * Once the database has been updated, we need to reset the update list and
+     * the counter, as they will no longer be valid.
+     * 
+     * @param database The new database to use.
+     */
+    void database(const data::InternalDatabase &database);
+
+
+    /**
+     * @brief Get the iteration limit for the simulator.
+     * 
+     * @return size_t The iteration limit
+     */
     inline size_t iteration_limit() { return _iteration_limit; }
+
+    /**
+     * @brief Set the iteration limit for the simulator
+     * 
+     * @param count The iteration limit
+     */
     inline void iteration_limit(const size_t count) { _iteration_limit = count; }
 
     void update_next();
     void update_all();
 
+    void reset_update_list();
     void reset_iteration_count();
 
     void update_by_id
--- src/sim/Simulator.cpp
+++ src/sim/Simulator.cpp
@@ -1,3 +1,12 @@
-
+/**
+ * @file Simulator.cpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief The definition file for the simulator object.
+ * @version 0.1
+ * @date 2022-01-09
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include <circsim/sim/Simulator.hpp>
