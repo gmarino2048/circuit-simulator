964b54fbf29fecdfb37bd20da8005fc0db3bd34b
Guy Marino <gmarino2048@gmail.com>
2022-03-09 12:58:57 -0500

    Add new files for external database

---
+++ inc/circsim/data/ExternalDatabase.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file ExternalDatabase.hpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief The object definition for the external database object
+ * @version 0.1
+ * @date 2022-03-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __CIRCSIM_DATA_EXTERNALDATABASE_HPP
+#define __CIRCSIM_DATA_EXTERNALDATABASE_HPP
+
+namespace circsim::data
+{
+
+class ExternalDatabase final
+{
+private:
+
+    
+
+};
+
+}
+
+#endif // #ifndef __CIRCSIM_DATA_EXTERNALDATABASE_HPP
---
+++ src/data/ExternalDatabase.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file ExternalDatabase.cpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief The object definitions for the External Database of the simulator.
+ * @version 0.1
+ * @date 2022-03-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <circsim/data/ExternalDatabase.hpp>
+
