258c8aec8e0b916ac2cfd82bd947d38f850e174b
Guy Marino <gmarino2048@gmail.com>
2022-08-11 13:22:47 -0400

    Generate template methods for wire

--- lib/data/src/ExternalStorage.cpp
+++ lib/data/src/ExternalStorage.cpp
@@ -386,6 +386,7 @@ sqlite3_stmt* ExternalStorage::_bind_values
 
 // Explicit instantiation of objects
 template bool ExternalStorage::_table_exists<circsim::components::Transistor>();
+template bool ExternalStorage::_table_exists<circsim::components::Wire>();
 
 template<class T>
 bool ExternalStorage::_table_exists()
@@ -494,6 +495,7 @@ ExternalStorage::~ExternalStorage()
 
 // Explicitly initialize count for transistor
 template size_t ExternalStorage::count<circsim::components::Transistor>() const;
+template size_t ExternalStorage::count<circsim::components::Wire>() const;
 
 template<class T>
 size_t ExternalStorage::count() const
