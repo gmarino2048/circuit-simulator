6e039f26ba7b8b14d500beccb701c1e602cdb8f2
Guy Marino <gmarino2048@gmail.com>
2022-08-11 20:04:46 -0400

    Add files for CircuitState implementation

---
+++ lib/components/include/circsim/components/CircuitState.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file CircuitState.hpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief This class contains a snapshot of the state of the circuit at some given
+ *        time. This information can be used to analyze the circuit operation at a
+ *        later date.
+ * @version 0.1
+ * @date 2022-08-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __CIRCSIM_COMPONENTS_CIRCUITSTATE_HPP
+#define __CIRCSIM_COMPONENTS_CIRCUITSTATE_HPP
+
+// C++ Stdlib Includes
+// (none)
+
+// Library Includes
+// (none)
+
+// Project Includes
+#include <circsim/components/Transistor.hpp>
+#include <circsim/components/Wire.hpp>
+
+#endif // #ifndef __CIRCSIM_COMPONENTS_CIRCUITSTATE_HPP
---
+++ lib/components/src/CircuitState.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file CircuitState.cpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief Implementation of the CircuitState object.
+ * @version 0.1
+ * @date 2022-08-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// C++ Stdlib Includes
+// (none)
+
+// Library Includes
+// (none)
+
+// Project Includes
+#include <circsim/components/CircuitState.hpp>
