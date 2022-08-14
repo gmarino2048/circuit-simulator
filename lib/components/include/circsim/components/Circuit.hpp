077e963e357740b50eb7a1758269f033e46e59b1
Guy Marino <gmarino2048@gmail.com>
2022-08-14 14:27:35 -0400

    Add circuit implementation files

---
+++ lib/components/include/circsim/components/Circuit.hpp
@@ -0,0 +1,40 @@
+/**
+ * @file Circuit.hpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief Definition of a complete circuit object used as part of
+ *        the simulator. It will contain each of the circuit's components
+ *        and allow the lists to be accessed in constant time.
+ * @version 0.1
+ * @date 2022-08-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef __CIRCSIM_COMPONENTS_CIRCUIT_HPP
+#define __CIRCSIM_COMPONENTS_CIRCUIT_HPP
+
+// C++ Stdlib Includes
+// (none)
+
+// Library Includes
+// (none)
+
+// Project Includes
+#include <circsim/components/Transistor.hpp>
+#include <circsim/components/Wire.hpp>
+
+namespace circsim::components
+{
+
+class Circuit final
+{
+private:
+
+    
+
+};  // end class Circuit
+
+}   // end namespace circsim::components
+
+#endif // #ifndef __CIRCSIM_COMPONENTS_CIRCUIT_HPP
---
+++ lib/components/src/Circuit.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file Circuit.cpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief Implementation file for the circuit object.
+ * @version 0.1
+ * @date 2022-08-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// C++ Stdlib Includes
+// (none)
+
+// Library Includes
+// (none)
+
+// Project Includes
+#include <circsim/components/Transistor.hpp>
+#include <circsim/components/Wire.hpp>
+
+#include <circsim/components/Circuit.hpp>
