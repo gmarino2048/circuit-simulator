a754db38e7a3b4aedcec29415e108c65d097f790
Guy Marino <gmarino2048@gmail.com>
2022-11-06 14:26:06 -0500

    Add test for half-adder and carry for bit 0

--- test/full-adder/full-adder.cpp
+++ test/full-adder/full-adder.cpp
@@ -52,11 +52,19 @@ void FullAdderTest::SetUp()
     circuit = circsim::data::JsonParser::parse(JSON_RELPATH);
 
     _simulator = new circsim::sim::Simulator(circuit);
+
+    // Quick and dirty hack to break in the circuit
+    _simulator->update_by_register<uint8_t>(REG_A_ID, 0b11111111, true);
+    _simulator->update_by_register<uint8_t>(REG_B_ID, 0b11111111, true);
+
+    _simulator->update_by_register<uint8_t>(REG_A_ID, 0b00000000, true);
+    _simulator->update_by_register<uint8_t>(REG_B_ID, 0b00000000, true);
 }
 
 
 void FullAdderTest::TearDown()
 {
+    circsim::components::Wire::RESET_CLASS();
     delete _simulator;
 }
 
@@ -76,12 +84,43 @@ TEST_F(FullAdderTest, Level1_InversionTest)
 
     for( uint8_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++ )
     {
-        _simulator->update_by_register(REG_A_ID, i, false);
-        _simulator->update_by_register(REG_B_ID, i, false);
+        _simulator->update_by_register<uint8_t>(REG_A_ID, i, false);
+        _simulator->update_by_register<uint8_t>(REG_B_ID, i, false);
 
         _simulator->update_all();
 
         EXPECT_EQ(not_a->value_unsigned<uint8_t>(), uint8_t(~i));
         EXPECT_EQ(not_b->value_unsigned<uint8_t>(), uint8_t(~i));
     }
+}
+
+
+TEST_F(FullAdderTest, Level1_BaseAdderTest)
+{
+    using Wire = circsim::components::Wire;
+
+    const uint64_t WIRE_OUT0_ID = 38;
+    const uint64_t WIRE_C0_ID = 39;
+
+    _simulator->update_by_register<uint8_t>(REG_A_ID, 0, false);
+    _simulator->update_by_register<uint8_t>(REG_B_ID, 0, false);
+    _simulator->update_all();
+
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_OUT0_ID)->low());
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_C0_ID)->low());
+
+    _simulator->update_by_register(REG_A_ID, 1, true);
+
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_OUT0_ID)->high());
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_C0_ID)->low());
+
+    _simulator->update_by_register(REG_B_ID, 1, true);
+    
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_OUT0_ID)->low());
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_C0_ID)->high());
+
+    _simulator->update_by_register(REG_A_ID, 0, true);
+
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_OUT0_ID)->high());
+    EXPECT_TRUE(_simulator->circuit().get<Wire>(WIRE_C0_ID)->low());
 }
