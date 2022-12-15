777ced63acd4d99eede7040f51344f60c138c06e
Guy Marino <gmarino2048@gmail.com>
2022-12-15 16:00:19 -0500

    Finish flip-flop dynamic test

--- test/flip-flop/flip-flop.cpp
+++ test/flip-flop/flip-flop.cpp
@@ -27,6 +27,11 @@ protected:
 
     const std::filesystem::path JSON_RELPATH = "flip-flop.json";
 
+    const uint64_t WIRE_D_ID = 1;
+    const uint64_t WIRE_CLK_ID = 3;
+    const uint64_t WIRE_Q_ID = 4;
+    const uint64_t WIRE_NOTQ_ID = 5;
+
     circsim::sim::Simulator* _simulator = nullptr;
 
     FlipFlopTest() = default;
@@ -34,6 +39,8 @@ protected:
 
     void SetUp() override;
     void TearDown() override;
+
+    bool wire_status(const uint64_t wire_id);
 
 };
 
@@ -51,7 +58,47 @@ void FlipFlopTest::TearDown()
     delete _simulator;
 }
 
-TEST_F(FlipFlopTest, DummyTestCase)
+bool FlipFlopTest::wire_status(const uint64_t wire_id)
 {
-    EXPECT_TRUE(true);
+    using Wire = circsim::components::Wire;
+
+    Wire* wire = _simulator->circuit().get<Wire>(wire_id);
+    return wire->high();
+}
+
+TEST_F(FlipFlopTest, FullComponentTest)
+{
+    using State = circsim::sim::Simulator::WireState;
+
+    // Starting state, make sure output is steady-state
+    _simulator->update_by_id(WIRE_D_ID, State::PULLED_LOW, false);
+    _simulator->update_by_id(WIRE_CLK_ID, State::PULLED_HIGH, false);
+    _simulator->update_all();
+
+    EXPECT_FALSE(wire_status(WIRE_Q_ID));
+    EXPECT_TRUE(wire_status(WIRE_NOTQ_ID));
+
+    // Set CLK low, verify outputs
+    _simulator->update_by_id(WIRE_CLK_ID, State::PULLED_LOW);
+
+    EXPECT_FALSE(wire_status(WIRE_Q_ID));
+    EXPECT_TRUE(wire_status(WIRE_NOTQ_ID));
+
+    // Set D high, verify outputs
+    _simulator->update_by_id(WIRE_D_ID, State::PULLED_HIGH);
+
+    EXPECT_FALSE(wire_status(WIRE_Q_ID));
+    EXPECT_TRUE(wire_status(WIRE_NOTQ_ID));
+
+    // Set CLK high, verify outputs changed
+    _simulator->update_by_id(WIRE_CLK_ID, State::PULLED_HIGH);
+
+    EXPECT_TRUE(wire_status(WIRE_Q_ID));
+    EXPECT_FALSE(wire_status(WIRE_NOTQ_ID));
+
+    // Set D low, verify outputs changed
+    _simulator->update_by_id(WIRE_D_ID, State::PULLED_LOW);
+
+    EXPECT_FALSE(wire_status(WIRE_Q_ID));
+    EXPECT_TRUE(wire_status(WIRE_NOTQ_ID));
 }
