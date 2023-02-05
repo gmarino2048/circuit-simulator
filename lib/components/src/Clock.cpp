0cb1b6b5398c0fd644d9abb03307692ed26a7d1d
Guy Marino <gmarino2048@gmail.com>
2023-02-05 16:21:55 -0500

    Add clock implementation file

---
+++ lib/components/src/Clock.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file Clock.cpp
+ * @author Guy Marino (gmarino2048@gmail.com)
+ * @brief Clock module responsible for tracking ticks and changing wire
+ *        status between ticks.
+ * @version 0.1
+ * @date 2023-02-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <circsim/components/Clock.hpp>
